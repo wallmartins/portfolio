c6ff57e18a96fbd6c2b01ed900062f47fde7f99b
Wallace Martinsm <wall_martins_@hotmail.com>
2025-04-11 15:12:46 -0300

    create environment project for start portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ["var(--font-orbitron)"],
+        button: ["var(--font-audiowide)"],
+        body: ["var(--font-space-grotesk)"],
+      },
+      blur: {
+        xl: "24px",
+      },
+    },
+  },
+  plugins: [],
+};
