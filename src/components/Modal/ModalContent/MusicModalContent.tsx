4a5125cc99e299222df23868c19d063a53954fab
wallmartins <wall_martins_@hotmail.com>
2025-04-25 17:44:32 -0300

    feat(musicModalContent): create iframe to spotify album on portfolio and adjust width modal of tools and music

--- src/components/Modal/ModalContent/MusicModalContent.tsx
+++ src/components/Modal/ModalContent/MusicModalContent.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const MusicModalContent = () => {
-  return <div className="text-[#c0cbcd]">Coming soon...</div>;
+  return (
+    <iframe
+      style={{ borderRadius: "12px" }}
+      src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO1IPOOk?utm_source=generator&theme=0"
+      width="100%"
+      height="512"
+      frameBorder="0"
+      allowFullScreen
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+      className="w-[352px] "
+    ></iframe>
+  );
 };
 
 export default MusicModalContent;
