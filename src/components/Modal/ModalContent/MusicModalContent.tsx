63958c334ed511423909b43f90cf9f4adafd0a4b
wallmartins <wall_martins_@hotmail.com>
2025-04-25 18:29:35 -0300

    feat(musicModalContent): create iframe to spotify album on portfolio and adjust width modal of tools and music

--- src/components/Modal/ModalContent/MusicModalContent.tsx
+++ src/components/Modal/ModalContent/MusicModalContent.tsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { SiSpotify } from "react-icons/si";
+import { TbArrowUpRight } from "react-icons/tb";
 
 const MusicModalContent = () => {
   return (
-    <iframe
-      style={{ borderRadius: "12px" }}
-      src="https://open.spotify.com/embed/album/5t78IIG7tiWQ9RUYApAJFG?utm_source=generator"
-      width="100%"
-      height="512"
-      frameBorder="0"
-      allowFullScreen
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      loading="lazy"
-      className="w-[352px] "
-    ></iframe>
+    <>
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/album/5t78IIG7tiWQ9RUYApAJFG?utm_source=generator"
+        width="100%"
+        height="512"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        className="w-[280px] "
+      ></iframe>
+      <a
+        href="https://open.spotify.com/intl-pt/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center pt-4 justify-between mt-2 border-t-1 border-[#73797b]/20"
+      >
+        <div className="flex items-center gap-3">
+          <SiSpotify className="text-3xl fill-[#73797b]" />
+          <div className="flex flex-col leading-tight">
+            <span className="text-sm font-medium text-white">More Music</span>
+            <span className="text-xs text-gray-400">Spotify</span>
+          </div>
+        </div>
+        <TbArrowUpRight className="text-sm text-gray-500" />
+      </a>
+    </>
   );
 };
 
