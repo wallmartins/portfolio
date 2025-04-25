2dd23aa6a88368600ac0e1a8913ce89f75a682b1
wallmartins <wall_martins_@hotmail.com>
2025-04-25 20:10:03 -0300

    fix(header.tsx): create the correct and updated clock to match with the date and hour of the browser

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,21 +34,7 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="text-xs text-[#c0cbcd] flex items-center gap-2 font-body">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
+        <div>
           <Clock />
         </div>
       </div>
